77bb811cad3e00307eb912949c4227f8da61d443
Felipe <felipe.olivera_@outlook.com>
2020-07-06 14:20:20 -0300

    Error treatment and response status

--- api/src/controller/TaskController.js
+++ api/src/controller/TaskController.js
@@ -2,14 +2,13 @@ const TaskModel = require('../model/TaskModel');
 
 class TaskController {
     async create(req, res) {
-
         try {
             const task = new TaskModel(req.body);
-            await task.save().then(() => {
-                return res.send().status(200);
+            await task.save().then(data => {
+                return res.status(200).json(data);
             })
-        } catch (error) {
-            return res.send().status(500);
+        } catch (err) {
+            return res.status(400).json(err.errors.title.properties.message);
         }
     }
 }
