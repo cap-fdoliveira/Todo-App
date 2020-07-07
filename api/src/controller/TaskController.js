c4ceba255c9db537c89b6165b2c598b259d0ad0e
Felipe <felipe.olivera_@outlook.com>
2020-07-07 12:35:33 -0300

    Finished crud to task component

--- api/src/controller/TaskController.js
+++ api/src/controller/TaskController.js
@@ -5,10 +5,43 @@ class TaskController {
         try {
             const task = new TaskModel(req.body);
             await task.save().then(data => {
-                return res.status(200).json(data);
+                return res.status(201).json(data);
             })
         } catch (err) {
-            return res.status(400).json(err.errors.title.properties.message);
+            return res.status(400).json(err);
+        }
+    }
+
+    async update(req, res) {
+        try {
+            await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true})
+            .then(data => {
+                return res.status(200).json(data);
+            });
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+
+    async delete(req, res) {
+        try {
+            await TaskModel.findOneAndDelete({'_id': req.params.id})
+            .then(data => {
+                return res.status(200).json(data);
+            });
+        } catch (err) {
+            return res.status(500).json(err);
+        }
+    }
+
+    async get(req, res) {
+        try {
+            await TaskModel.find()
+            .then(data => {
+                return res.status(200).json(data);
+            });
+        } catch (err) {
+            return res.status(500).json(err);
         }
     }
 }
--- api/src/routes/TaskRoutes.js
+++ api/src/routes/TaskRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const TaskController = require('../controller/TaskController');
 
 router.post('/', TaskController.create);
+router.put('/:id', TaskController.update);
+router.delete('/:id', TaskController.delete);
+router.get('/', TaskController.get);
 
 module.exports = router;
