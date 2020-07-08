3c4a035978cab65a41a20e0f6d539844769b3483
Felipe <felipe.olivera_@outlook.com>
2020-07-08 18:03:59 -0300

    Functions updateTaskStatus and taskLate

--- api/src/controller/TaskController.js
+++ api/src/controller/TaskController.js
@@ -1,65 +1,97 @@
 const TaskModel = require('../model/TaskModel');
 
+const current = new Date();
+
 class TaskController {
     async create(req, res) {
-        try {
-            const task = new TaskModel(req.body);
-            await task.save().then(data => {
-                return res.status(201).json(data);
-            })
-        } catch (err) {
-            return res.status(400).json(err);
-        }
+
+        const task = new TaskModel(req.body);
+        await task.save().then(data => {
+            return res.status(201).json(data);
+        })
+        .catch(err => {
+            return res.status(500).json(err);
+        });
     }
 
     async update(req, res) {
-        try {
-            await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true})
-            .then(data => {
-                return res.status(200).json(data);
-            });
-        } catch (err) {
+
+        await TaskModel.findByIdAndUpdate(
+            { '_id': req.params.id }, req.body, { new: true }
+        ).then(data => {
+            return res.status(200).json(data);
+        })
+        .catch(err => {
             return res.status(500).json(err);
-        }
+        });
     }
 
     async delete(req, res) {
-        try {
-            await TaskModel.deleteOne({'_id': req.params.id})
-            .then(data => {
-                return res.status(200).json(data);
-            });
-        } catch (err) {
+
+        await TaskModel.deleteOne(
+            { '_id': req.params.id }
+        ).then(data => {
+            return res.status(200).json(data);
+        })
+        .catch(err => {
             return res.status(500).json(err);
-        }
+        });
     }
 
     async all(req, res) {
-        try {
-            const mac = req.body;
-            await TaskModel.find({ macaddress: {'$in': mac.macaddress} })
-            .sort('date')
+
+        await TaskModel.find(
+            { macaddress: { '$in': req.body.macaddress } }
+        ).sort('date')
             .then(data => {
                 return res.status(200).json(data);
+            })
+            .catch(err => {
+                return res.status(500).json(err);
             });
-        } catch (err) {
-            return res.status(500).json(err);
-        }
     }
 
     async fetchTaskById(req, res) {
-        try {
-            await TaskModel.findById(req.params.id)
+
+        await TaskModel.findById(req.params.id)
             .then(data => {
                 if (data) {
                     return res.status(200).json(data);
                 }
-                return res.status(400).json({err: 'Task not found'})
+                return res.status(400).json({ err: 'Task not found' })
+            })
+            .catch(err => {
+                return res.status(500).json(err);
             });
-        } catch (err) {
-            return res.status(500).json(err);
-        }
+    }
 
+    async updateTaskStatus(req, res) {
+
+        await TaskModel.findOneAndUpdate(
+            { '_id': req.params.id },
+            { 'done': req.params.done },
+            { new: true }
+        ).then(data => {
+            return res.status(200).json(data);
+        })
+        .catch(err => {
+            return res.status(500).json(err);
+        });
+    }
+
+    async taskLate(req, res) {
+
+        await TaskModel.find({
+            'date': { '$lt': current },
+            'macaddress': { '$in': req.body.macaddress }
+        })
+        .sort('date')
+            .then(data => {
+                return res.status(200).json(data);
+            })
+            .catch(err => {
+                return res.status(500).json(err);
+        });
     }
 }
 
--- api/src/routes/TaskRoutes.js
+++ api/src/routes/TaskRoutes.js
@@ -7,5 +7,7 @@ router.put('/:id', TaskController.update);
 router.delete('/:id', TaskController.delete);
 router.get('/all', TaskController.all);
 router.get('/:id', TaskController.fetchTaskById);
+router.put('/:id/:done', TaskController.updateTaskStatus);
+router.get('/late', TaskController.taskLate);
 
 module.exports = router;
