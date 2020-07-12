fdf82ba04b8ca186dd7c4d96cd4619c92fa778de
Felipe <felipe.olivera_@outlook.com>
2020-07-12 18:35:29 -0300

    Header component - styles

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { Container, Logo, Menu } from './styles';
+import logo from '../../assets/logo-todo2.png';
+import { Container, Logotype, Menu } from './styles';
+import { FiBell } from 'react-icons/fi';
 
 
 function Header() {
     return (
         <Container>
-            <Logo img="../../assets/ToDo-app.png"/>
+            <Logotype>
+                <img src={logo} alt="logotipo do app" />
+            </Logotype>
             <Menu>
-
+                <a href="#">Início</a>
+                <span className="divider" />
+                <a href="#">Nova Tarefa</a>
+                <span className="divider" />
+                <a href="#">Sincronizar celular</a>
+                <span className="divider" />
+                <a href="#" id="notification">
+                    <FiBell size={28}/>
+                    <span>3</span>
+                </a>
             </Menu>
         </Container>
     );
--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -1,10 +1,62 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    display: flex;
     width: 100%;
     height: 70px;
-    background: #483D8B;
+    background: #363636	;
+    border-bottom: 5px solid #836FFF;
 `;
 
-export const Logo = styled.div``;
-export const Menu = styled.div``;
+export const Logotype = styled.image`
+    display: flex;
+    align-items: center;
+    width: 50%;
+    height: 70px;
+    margin: 0 10px;
+`;
+
+export const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    width: 50%;
+    height: 70px;
+    margin: 0 20px;
+
+    a {
+        color: #dcdcdc;
+        font-weight: bold;
+        text-decoration: none;
+        font-size: 18px;
+        margin: 0 10px;
+
+        &:hover{
+            color: #708090;
+        }
+    }
+    #notification {
+            span {
+            background: #dcdcdc;
+            color: #836FFF;
+            padding: 1px 6px;
+            border-radius: 50%;
+            position: relative;
+            top: -25px;
+            right: 10px;
+            font-size: 12px;
+        }
+
+        &:hover {
+            opacity: 0.5;
+        }
+    }
+
+    .divider::after{
+        content: "|";
+        color: #dcdcdc;
+        margin: 0 10px;
+        font-size: 18px;
+    }
+
+`;
