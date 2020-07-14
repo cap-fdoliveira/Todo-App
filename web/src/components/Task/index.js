a631c0471e0502654f960dd80d19c26e611c4137
Felipe <felipe.olivera_@outlook.com>
2020-07-13 22:20:17 -0300

    Task card - style

--- web/src/components/Task/index.js
+++ web/src/components/Task/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { Container } from './styles'
+import { Container, TaskCard, Description, Title, TaskDate, Hour } from './styles';
+import { FiCalendar } from 'react-icons/fi';
 
 function Task() {
     return (
         <Container>
-            
+            <TaskCard>
+                <Description>
+                    <FiCalendar size={50} color="#dcdcdc"/>
+                    <Title>Terminar esse programa</Title>
+                </Description>
+                <TaskDate>
+                    11/08/2020
+                    <Hour>16:00</Hour>
+                </TaskDate>
+            </TaskCard>
         </Container>
     );
 }
--- web/src/components/Task/styles.js
+++ web/src/components/Task/styles.js
@@ -2,5 +2,50 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
     width: 100%;
-    height: 55em;
+    height: auto;
+    margin-top: 40px;
+    display: flex;
+    justify-content: space-between;
+`;
+
+export const TaskCard = styled.div`
+    width: 20%;
+    height: 12em;
+    padding: 15px;
+    margin: auto;
+    border-radius: 10px;
+    background: #363636;
+`;
+
+export const Description = styled.div`
+    width: 100%;
+    height: 9em;
+    border-radius:10px;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    cursor: pointer;
+`;
+
+export const Title = styled.div`
+    margin-bottom: 5px;
+    color: #dcdcdc;
+    font-size: 20px;
+`;
+
+export const TaskDate = styled.div`
+    width: 100%;
+    height: 3em;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #dcdcdc;
+    font-size: 16px;
+    border-top: 5px solid #836FFF;
+`;
+
+export const Hour = styled.div`
+    color: #dcdcdc;
+    font-size: 16px;
 `;
